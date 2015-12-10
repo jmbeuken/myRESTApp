9ece9c48b4c5badca7dda05c8a8b105e2c601352
Jean-Michel Beuken <jean-michel.beuken@uclouvain.be>
2015-12-10 12:10:31 +0100

    x-key not needed

--- server/middlewares/validateRequest.js
+++ server/middlewares/validateRequest.js
@@ -11,7 +11,7 @@ module.exports = function(req, res, next) {
   //if(req.method == 'OPTIONS') next();
 
   var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
-  var key = (req.body && req.body.x_key) || (req.query && req.query.x_key) || req.headers['x-key'];
+  var key = (req.body && req.body.x_key) || (req.query && req.query.x_key);
 
   if (token || key) {
     try {
